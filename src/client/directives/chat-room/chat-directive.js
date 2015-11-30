bb478ab3419de4fd583d739861767ad8cae3bbf1
Sarah Turek <snturek@gmail.com>
2015-11-30 11:09:36 -0700

    chat working?

--- src/client/directives/chat-room/chat-directive.js
+++ src/client/directives/chat-room/chat-directive.js
@@ -3,22 +3,35 @@ app.directive('chatRoom', function(){
       restrict: 'E',
       templateUrl: 'directives/chat-room/chat-room.html',
       controller: ['$scope', 'UserServices', '$http', function($scope, UserServices, $http) {
-
-        $scope.message = "HEYYYY";
-
+  //ONLY FOR TESTING, DELETE ME
+        UserServices.storeUser({username: 'test'});
+        //start socket
         var socket = io.connect();
-        console.log('whatttt');
 
-        socket.emit('test');
+        //get user, add to online users
+        var user = UserServices.getUser();
+        socket.emit('user', user.username);
 
+        //wrap elements in angular
+        var chatUl = angular.element(document.querySelector('#chat-ul'));
+        var onlineUsers = angular.element(document.querySelector('#user-ul'));
 
-        socket.on('sup', function(){
-          console.log("HIIIIII");
+        //send message
+        $scope.sendMessage = function(){
+          socket.emit('message-sent', $scope.chatInput);
+          $scope.chatInput = '';
+        };
+
+        //append message after hitting socket
+        socket.on('message-received', function(message){
+          chatUl.append('<li>&nbsp' + user.username + ': ' + message + '</li>');
         });
 
-
-
-
+        socket.on('online-users', function(users){
+          for (var i = 0; i < users.length; i++) {
+            onlineUsers.append('<li>' + users[i]+'</li>');
+          }
+        });
 
 
 
