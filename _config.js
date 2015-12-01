efb7681b5566082413c857ed4eb38fc5757d2ad9
Sarah Turek <snturek@gmail.com>
2015-12-01 15:44:27 -0700

    mongolabs

--- _config.js
+++ _config.js
@@ -1,5 +1,5 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'my-precious',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/teacher-student',
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://heroku_nzmv06sq:orn79vo2rdhhqcp0adei36kguk@ds059634.mongolab.com:59634/heroku_nzmv06sq' || 'mongodb://localhost/teacher-student',
   SALT_WORK_FACTOR: 10,
 };
