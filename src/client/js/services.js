763dcf3b691de04e5626b7679761150138d3fe42
Sarah Turek <snturek@gmail.com>
2015-12-08 13:49:05 -0700

    added test words for demo

--- src/client/js/services.js
+++ src/client/js/services.js
@@ -173,7 +173,7 @@ app.factory('UserServices', ['$http', function($http){
 
   //filters out vulgar language
   function languageFilter(str, teacher){
-    var profanities = ['shit', 'fuck', 'ass', 'bastard', 'bitch', 'cock', 'cunt', 'kunt', 'dick', 'dyke', 'dike', 'fag', 'faggit', 'faggot', 'fagit', 'fagot', 'damn', 'dammit', 'asshole', 'whore', 'homo', 'nigga', 'nigger', 'pussy', 'queer', 'slut', 'whore', 'tit', 'titty', 'twat', 'fucka', 'retard', 'scrote', 'scrotum', 'titties', 'fucker', 'penis', 'vagina', 'shithead', 'blowjob', 'cum', 'buttplug', 'buttplugs', 'dipshit', 'queef', 'bitches', 'bitchy', 'jizz', 'cocksucker', 'cocksuckers', 'jackass', 'jackasses'];
+    var profanities = ['shit', 'fuck', 'ass', 'bastard', 'bitch', 'cock', 'cunt', 'kunt', 'dick', 'dyke', 'dike', 'fag', 'faggit', 'faggot', 'fagit', 'fagot', 'damn', 'dammit', 'asshole', 'whore', 'homo', 'nigga', 'nigger', 'pussy', 'queer', 'slut', 'whore', 'tit', 'titty', 'twat', 'fucka', 'retard', 'scrote', 'scrotum', 'titties', 'fucker', 'penis', 'vagina', 'shithead', 'blowjob', 'cum', 'buttplug', 'buttplugs', 'dipshit', 'queef', 'bitches', 'bitchy', 'jizz', 'cocksucker', 'cocksuckers', 'jackass', 'jackasses', 'bumface'];
 
     var symbols = '!@#$%^&*©æ®√∞Ω∫∆¥ƒ∂ß';
     var arr = str.split(' ');
